b1b588952694d827af4754cecaa948937d1908f1
Huseyin Karatas <huseyin.karatas@metu.edu.tr>
2023-08-31 20:59:29 +0300

    Created TodoList Component and its Model & Migration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Livewire\Counter;
+use App\Livewire\TodoList;
 use App\Livewire\Calculator;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,5 @@ Route::get('/', function () {
 
 Route::get('/calculator' , Calculator::class);
 Route::get('/counter' , Counter::class);
+Route::get('/todo-list' , TodoList::class);
+
