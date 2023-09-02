272a5fa51ddb1a241ae30e90d527cd7b717d95e3
Huseyin Karatas <huseyin.karatas@metu.edu.tr>
2023-09-02 19:30:53 +0300

    Configurated Migrations, Models, and Factories. Made Seeding.

--- database/migrations/2023_09_02_160455_create_countries_table.php
+++ database/migrations/2023_09_02_160455_create_countries_table.php
@@ -1,18 +1,20 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
+    
     public function up(): void
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('continent_id')->references('id')->on('continents');
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,33 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Country;
+use App\Models\Continent;
+use Database\Factories\ContinentFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $continents = [
+            ['id' => 1, 'name' => 'Europe',],
+            ['id' => 2, 'name' => 'Asia'],
+            ['id' => 3, 'name' => 'Africa',],
+            ['id' => 4, 'name' => 'South America',],
+            ['id' => 5, 'name' => 'North America',],
+        ];
+
+        foreach ($continents as $continent) {
+            \App\Models\Continent::create($continent)
+            ->each(function ($c) {
+                $c->countries()->saveMany(
+                    Country::factory()->count(5)->make()
+                );
+            });
+
+        }
+        
     }
 }
